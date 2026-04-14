69eccbddadf197ff5329c90174261793d1cbbca8
RayKang8 <raykang2004@gmail.com>
2026-04-14 00:33:38 -0400

    logo change

--- components/YokeLogo.tsx
+++ components/YokeLogo.tsx
@@ -1,12 +1,15 @@
-import YokeLogoSvg from '../assets/yoke_logo.svg';
+import { Image } from 'react-native';
 
 interface Props {
   size?: number;
 }
 
-// Viewbox is 680×690. Height scales proportionally with size.
 export default function YokeLogo({ size = 160 }: Props) {
-  const height = (690 / 680) * size;
-
-  return <YokeLogoSvg width={size} height={height} />;
+  return (
+    <Image
+      source={require('../assets/Yoke.png')}
+      style={{ width: size, height: size }}
+      resizeMode="contain"
+    />
+  );
 }
